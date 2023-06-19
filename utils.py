02e82c1694a565bc749c64ed42f3051faf1d1dcc
Shura <julian.domeneghini@gmail.com>
2023-06-18 21:32:36 -0300

    Alterado nossa REST API para realizar autenticação no banco de dados. Em models foi criado tabela de usuários com login e senha para autenticação nas APIs. Incluído em requeriments a extensão flask-httauth.

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
-from models import Pessoas
+from models import Pessoas, Usuarios
+
 
 # Insere dados na tabela pessoa
 def insere_pessoas():
@@ -24,8 +25,19 @@ def exclui_pessoa():
     pessoa = Pessoas.query.filter_by(nome='Felipe').first()
     pessoa.delete()
 
+def insere_usuario(login, senha):
+    usuario = Usuarios(login=login, senha=senha)
+    usuario.save()
+
+def consulta_todos_usuarios():
+    usuarios = Usuarios.query.all()
+    print(usuarios)
+
 if __name__ == '__main__':
+    insere_usuario('julian', '1234')
+    insere_usuario('domeneghini', '4321')
+    consulta_todos_usuarios()
     # insere_pessoas()
-    #altera_pessoa()
-    exclui_pessoa()
-    consulta_pessoas()
+    # altera_pessoa()
+    # exclui_pessoa()
+    # consulta_pessoas()
